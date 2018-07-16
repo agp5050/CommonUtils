527826382e8f24d93f0f3f9769fcdad344e63b6e
philip <agp5050554@163.com>
2018-07-16 18:48:06 +0800

    优先队列PriorityQueue

---
+++ PriorityQueue.java
@@ -0,0 +1,39 @@
+  
+  //以默认DEFAULT_INITIAL_CAPACITY = 11的object数组作为存储结构。
+  transient Object[] queue
+  
+  //每存一个元素的时候就对新元素进行比较插值
+  //add(E e) -->offer(e)-->siftUp(i, e)-->siftUpUsingComparator(k, x)|siftUpComparable(k, x)
+  private void siftUp(int k, E x) {
+        if (comparator != null)
+            siftUpUsingComparator(k, x);
+        else
+            siftUpComparable(k, x);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUpComparable(int k, E x) {
+        Comparable<? super E> key = (Comparable<? super E>) x;
+        while (k > 0) {
+            int parent = (k - 1) >>> 1;
+            Object e = queue[parent];
+            if (key.compareTo((E) e) >= 0)
+                break;
+            queue[k] = e;
+            k = parent;
+        }
+        queue[k] = key;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void siftUpUsingComparator(int k, E x) {
+        while (k > 0) {
+            int parent = (k - 1) >>> 1;
+            Object e = queue[parent];
+            if (comparator.compare(x, (E) e) >= 0)
+                break;
+            queue[k] = e;
+            k = parent;
+        }
+        queue[k] = x;
+    }
