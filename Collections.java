c48639bb4b33ddaa9f09ef1520f1249f7be439e3
philip <agp5050554@163.com>
2018-07-24 15:11:21 +0800

    Update Collections.java

--- Collections.java
+++ Collections.java
@@ -8,6 +8,17 @@
     //NB l.set(i, l.set(j,l.get(i)));  l.set(j,)返回j位置的之前的值。
     
     
+
+    @SuppressWarnings("unchecked")
+    public static <T> int binarySearch(List<? extends T> list, T key, Comparator<? super T> c) {
+        if (c==null)
+            return binarySearch((List<? extends Comparable<? super T>>) list, key);
+
+        if (list instanceof RandomAccess || list.size()<BINARYSEARCH_THRESHOLD)//根据有没有整数索引分成两种查询
+            return Collections.indexedBinarySearch(list, key, c);  
+        else
+            return Collections.iteratorBinarySearch(list, key, c); 
+    }
        
     private static <T> int indexedBinarySearch(List<? extends Comparable<? super T>> list, T key) {
         int low = 0;
@@ -17,6 +28,26 @@
             int mid = (low + high) >>> 1;// 写的mid 迭代比较好。 无符号左移一位，速度快
             Comparable<? super T> midVal = list.get(mid);
             int cmp = midVal.compareTo(key);
+
+            if (cmp < 0)
+                low = mid + 1;
+            else if (cmp > 0)
+                high = mid - 1;
+            else
+                return mid; // key found
+        }
+        return -(low + 1);  // key not found
+    }
+
+    private static <T> int iteratorBinarySearch(List<? extends T> l, T key, Comparator<? super T> c) {
+        int low = 0;
+        int high = l.size()-1;
+        ListIterator<? extends T> i = l.listIterator(); //AbstractList 实现了这个listIterator方法，初始cursor＝0
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            T midVal = get(i, mid);
+            int cmp = c.compare(midVal, key);
 
             if (cmp < 0)
                 low = mid + 1;
