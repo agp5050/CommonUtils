b31f3d06ba15c9ca8b2b51ef1cc2fd64e3c3fb28
philip <agp5050554@163.com>
2019-04-02 19:54:00 +0800

    Update AutoUpdateConfigs.java

--- AutoUpdateConfigs.java
+++ AutoUpdateConfigs.java
@@ -1,6 +1,5 @@
-
-
-import com.jffox.cloud.config.Configs;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.te.datalake.configs.Configs;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileNotFoundException;
@@ -12,13 +11,14 @@ import java.util.LinkedHashMap;
 public class AutoUpdateConfigs<T>{
     private T configs;
     private String filePath;
-    public AutoUpdateConfigs(T configs,String path){
+    public AutoUpdateConfigs(T configs, String path){
         this.configs=configs;
         this.filePath=path;
     }
 
     public void updateBean() {
         Yaml yaml=new Yaml();
+        boolean ifModified=false;
         try {
             HashMap<String,Object> hashMap = yaml.loadAs(new FileReader(filePath), HashMap.class);
             HashMap<String,Object> hashMapN=new HashMap<>();
@@ -33,11 +33,20 @@ public class AutoUpdateConfigs<T>{
             for (Field field:declaredFields){
                 field.setAccessible(true);
                 Object newValue = hashMapN.get(field.getName());
-                if(newValue!=null){
-                    field.set(configs,newValue+"");
+                if (newValue!=null){
+                    if (newValue.equals(field.get(configs))){
+                        continue;
+                    }
+                    ifModified=true;
+                    field.set(configs,newValue);
                 }
+
             }
-            System.out.println(configs+"-----new Bean");
+            //如果有修改打印到控制台
+            if (ifModified){
+                System.out.println(configs+"-----new Bean");
+
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -52,11 +61,38 @@ public class AutoUpdateConfigs<T>{
     public static void main(String[] args) {
         Configs configs = new Configs();
         System.out.println(configs);
-        String path=System.getProperty("user.dir")+"\\src\\main\\resources\\application-prod.yml";
+        String path=System.getProperty("user.dir")+"\\src\\main\\resources\\application-test.yml";
         System.out.println(path);
         AutoUpdateConfigs<Configs> autoUpdateConfigs=new AutoUpdateConfigs<>(configs,path);
         autoUpdateConfigs.updateBean();
         System.out.println(configs);
+    }
+}
+
+
+
+@Component
+@Slf4j
+class AutoUpdateThread extends Thread {
+    @Autowired
+    Configs configs;
+    public  static  ScheduledExecutorService cronExecutorcronExecutor = Executors
+            .newScheduledThreadPool(2,new ThreadFactoryBuilder().setNameFormat("dxddpost-%d").build());
+
+    public synchronized static void newExecutor(){
+        cronExecutorcronExecutor= Executors
+                .newScheduledThreadPool(2,new ThreadFactoryBuilder().setNameFormat("dxddpost-%d").build());
+    }
+    public void run(){
+        log.warn("update bean submitted...");
+        cronExecutorcronExecutor.scheduleAtFixedRate(()->{
+            String filePath=System.getProperty("user.dir") + "/conf/application-prod.yml";
+            AutoUpdateConfigs<Configs> configsAutoUpdateConfigs = new AutoUpdateConfigs(configs,filePath);
+            configsAutoUpdateConfigs.updateBean();
+        },Long.valueOf(configs.getDelay()),Long.valueOf(configs.getInterval()),TimeUnit.MILLISECONDS);
+    }
+    public AutoUpdateThread(Configs configs){
+        this.configs=configs;
     }
 }
 
